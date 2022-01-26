c2a9c2cd49b7e355f024d97a74abc86e7818b66b
Christian C <x10an14@gmail.com>
2022-01-26 17:06:42 +0100

    Refactor: Replace `nixos` input references with `nixpkgs`

--- flake.nix
+++ flake.nix
@@ -1,9 +1,17 @@
 {
   # Inspired/stolen from: https://hoverbear.org/blog/nix-flake-live-media/
   description = "x10an4's NixOS Live ISO image";
-  inputs.nixos.url = "github:nixos/nixpkgs/nixos-21.11";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+  };
 
-  outputs = { self, nixos }: {
+  outputs = {
+    self
+    , nixpkgs
+    , ...
+  }: let
+    systemArch = "x86_64-linux";
+  in {
     nixosConfigurations = let
       # Shared base configuration.
       baseConfig = {
@@ -13,13 +21,13 @@
         ];
       };
     in {
-      installIso = nixos.lib.nixosSystem {
+      installIso = nixpkgs.lib.nixosSystem {
         inherit (baseConfig) system;
         modules = baseConfig.modules ++ [
-          "${nixos}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
         ];
       };
-      initialSystem = nixos.lib.nixosSystem {
+      initialSystem = nixpkgs.lib.nixosSystem {
         inherit (baseConfig) system;
         modules = baseConfig.modules ++ [
           # Modules for installed systems only.
