a08065a5e5f07dbf4aa89dd30e258fd9a6e08889
Christian C <x10an14@gmail.com>
2022-01-26 21:59:23 +0100

    Temp commit - unable to build =(

---
+++ config/encrypted-boot.nix
@@ -0,0 +1,44 @@
+{ config, ... }:
+
+let
+  btrfsMountOptions = [
+    "noatime"
+    "compress=zstd"
+  ];
+
+  # TODO: receive the two below variables as input from another file, make this a module
+  # AKA don't hard-code...
+  fsDevices = {
+    efiPartitionUuid = "/dev/disk/by-partuuid/XXX";
+    swap.unlockedUuid = "/dev/disk/by-partuuid/XXX";
+    root.unlockedUuid = "/dev/disk/by-partuuid/XXX";
+  };
+  btrfsDevices = {
+    "/" = { subvol = "@"; neededForBoot = true; };
+    "/containercow" = { subvol = "@containercow"; };
+    "/home" = { subvol = "@home"; };
+    "/nix" = { subvol = "@nix"; };
+    "/persist" = { subvol = "@persist"; neededForBoot = true; };
+    "/var/log" = { subvol = "@log"; neededForBoot = true; };
+  };
+in {
+  fileSystems = builtins.ListToAttrs (
+    builtins.mapAttrs (
+      mountPoint: btrfsDevice: {
+        name = "${mountPoint}";
+        value = {
+          fsType = "btrfs";
+          options = [ "subvol=${btrfsDevice.subvol}" ] ++ btrfsMountOptions;
+        } // btrfsDevice;
+      }
+    ) // {
+      "/efi" = {
+        device = fsDevices.efiPartitionUuid;
+        fsType = "vfat";
+      };
+    }
+  ) btrfsDevices;
+
+
+  swapDevices = [ { device = fsDevices.swapUnlocked; } ];
+}
