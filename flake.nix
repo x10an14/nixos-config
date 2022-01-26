f3c683e2ee7c99b2fd94f01a3df2d0fef20e4e20
Christian C <x10an14@gmail.com>
2022-01-26 15:33:32 +0100

    Feature: Now pull in versioned `nix` binary supporting nix flakes!
    While _not_ running the entire system on unstable!
    Bonus: Can also use syntax `pkgs.unstable.<package>` from now on =)
    
    Many thanks to @Jan Tojnar! (https://matrix.to/#/!KqkRjyTEzAGRiZFBYT:nixos.org/$d_6qpxQoN6TwZvlvf5Pjl2QpT6no0m74kU-TDUVbGkA?via=nixos.org&via=matrix.org&via=nixos.dev)

--- flake.nix
+++ flake.nix
@@ -5,19 +5,34 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
   };
 
-  outputs = {
+  outputs = inputs@{
     self
     , nixpkgs
     , ...
   }: let
     systemArch = "x86_64-linux";
+
+    # Allow the referencing of unstable packages with `pkgs.unstable.<package>`:
+    # Inspired by: https://dzone.com/articles/nixos-home-manager-on-native-nix-flake-installatio
+    overlay-unstable = final: prev: {
+      inherit systemArch;
+      unstable = inputs.nixpkgs-unstable.legacyPackages."${systemArch}";
+    };
+    pkgs = import nixpkgs {
+      inherit systemArch overlay-unstable;
+      system = systemArch;
+      overlays = [ overlay-unstable ];
+    };
   in {
     nixosConfigurations = let
       # Shared base configuration.
       baseConfig = {
-        system = "x86_64-linux";
+        system = systemArch;
         modules = [
+          {nixpkgs.pkgs = pkgs;}
+
           # Common system modules...
+          ./base/common/nix-store.nix
         ];
       };
     in {
